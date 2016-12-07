5f3b6e6e95a932e4e74c7de8768d9f0563436b80
Christopher Impicciche <cimpicci@gmail.com>
2016-12-07 16:14:01 -0600

    adds dynamic listing of data passed from API

--- public/script.js
+++ public/script.js
@@ -1,6 +1,22 @@
 var app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    students: null
+  },
+  created: function() {
+    this.fetchStudents();
+  },
   computed: {},
-  methods: {}
+  methods: {
+    fetchStudents: function() {
+      var xhr = new XMLHttpRequest();
+      var self = this;
+      xhr.open('GET', '/students');
+      xhr.onload = function() {
+        self.students = JSON.parse(xhr.responseText);
+        console.log(self.students);
+      }
+      xhr.send();
+    }
+  }
 })
