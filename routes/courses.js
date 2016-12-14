759d4985c1c1463a6c201ad41358939762d288b3
Christopher Impicciche <cimpicci@gmail.com>
2016-12-14 09:17:09 -0600

    courses now uses rendering

--- routes/courses.js
+++ routes/courses.js
@@ -5,7 +5,7 @@ module.exports = function(express, db) {
   router.route('/')
     .get( (req,res) => {
       // TODO make this flexible / configurable
-      db.run('MATCH (a:Article)-[*]->(b) RETURN a,b')
+      db.run('MATCH (n:Course) RETURN n SKIP 0 LIMIT 25')
         .then( result => {
           var data = [];
           result.records.forEach(record => {
@@ -13,7 +13,9 @@ module.exports = function(express, db) {
             data.push(record._fields[0]);
           })
           db.close()
-          res.send(data);
+          // res.send(data);
+          console.log(data);
+          res.render('courses', {data:data})
         })
         .catch( err => {
           console.log(err);
@@ -29,7 +31,24 @@ module.exports = function(express, db) {
 
   router.route('/:title')
     .get( (req,res) => {
-      res.send(`display page for ${req.params.title}`)
+      db.run(`MATCH (c:Course {title: "${req.params.title}"})<--(q:Quarter {}) return q`)
+        .then( result => {
+          var data =[];
+          console.log(result);
+          result.records.forEach(record=>{
+            console.log('REC:',record._fields[0]);
+            data.push(record._fields[0]);
+          })
+          console.log(data);
+          res.render('course', {data:data, title:req.params.title})
+        })
+        .catch(err => {
+          console.log(err);
+          res.send(err)
+        })
+      // get quarters for course
+
+      // res.send(`display page for ${req.params.title}`)
     })
 
   router.route('/:title/edit')
