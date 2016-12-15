26dfb4c44872d926ab72af29f17ae6f7d64cea17
Christopher Impicciche <cimpicci@gmail.com>
2016-12-15 10:58:44 -0600

    removes db helper api methods from db config

--- config/db.js
+++ config/db.js
@@ -2,36 +2,5 @@ module.exports = function (config, neo4j) {
   var driver = neo4j.v1.driver(config.db.address, neo4j.v1.auth.basic(config.db.user, config.db.password));
   var session = driver.session();
 
-
-// DATABASE FUNCTIONS
-
-
-function getNodeByAttribute(label, attribute){
-  return function(search, cb) {
-    var query = `MATCH (n:${label}) WHERE n.${attribute}=${search}`
-    session.run(query)
-      .then( result => {
-        cb(result)
-        session.close()
-      })
-      .catch(err=> {
-        if (err) throw err;
-      })
-  }
-}
-
-
-var getProgramByTitle = getNodeByAttribute('Program', 'title');
-var getOutcomeByName = getNodeByAttribute('Outcome', 'name');
-
-
-var api = {
-  getProgramByTitle: getProgramByTitle,
-  getOutcomeByName: getOutcomeByName
-}
-
-  return {
-    session: session,
-    api: api
-  }
+  return session;
 }
