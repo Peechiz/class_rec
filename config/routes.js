52fd4a4d7886b2691214911a48d559136821eb67
Christopher Impicciche <cimpicci@gmail.com>
2016-12-15 11:14:29 -0600

    renames courses to program

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,5 @@
 module.exports = function(app, config, db, express) {
 
-  var students = require('../routes/students')(express,db);
-  var courses = require('../routes/courses')(express,db);
-  app.use('/students', students);
-  app.use('/courses', courses);
+  var program = require('../routes/program')(express,db);
+  app.use('/program', program);
 }
--- routes/program.js
+++ routes/program.js
@@ -1,18 +1,21 @@
+const api = require('../neo4j/query_builder');
+
 module.exports = function(express, db) {
 
+  var session = db
   var router = express.Router();
 
   router.route('/')
     .get( (req,res) => {
       // TODO make this flexible / configurable
-      db.run('MATCH (n:Course) RETURN n SKIP 0 LIMIT 25')
+      session.run('MATCH (n:Program) RETURN n SKIP 0 LIMIT 25')
         .then( result => {
           var data = [];
           result.records.forEach(record => {
             console.log('REC:',record._fields[0]);
             data.push(record._fields[0]);
           })
-          db.close()
+          session.close()
           // res.send(data);
           console.log(data);
           res.render('courses', {data:data})
@@ -31,24 +34,15 @@ module.exports = function(express, db) {
 
   router.route('/:title')
     .get( (req,res) => {
-      db.run(`MATCH (c:Course {title: "${req.params.title}"})<--(q:Quarter {}) return q`)
-        .then( result => {
-          var data =[];
-          console.log(result);
-          result.records.forEach(record=>{
-            console.log('REC:',record._fields[0]);
-            data.push(record._fields[0]);
+        var query = api.get.programByTitle(req.params.title)
+        session.run(query)
+          .then( result => {
+            res.send(result)
           })
-          console.log(data);
-          res.render('course', {data:data, title:req.params.title})
-        })
-        .catch(err => {
-          console.log(err);
-          res.send(err)
-        })
-      // get quarters for course
-
-      // res.send(`display page for ${req.params.title}`)
+          .catch( err => {
+            console.log(err);
+            res.send(err)
+          })
     })
 
   router.route('/:title/edit')
