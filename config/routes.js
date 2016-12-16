9be201dcbdcccde235ca67d7b0a2e4f218b80399
Christopher Impicciche <cimpicci@gmail.com>
2016-12-16 16:42:46 -0600

    separates out api routes from render routes

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,7 @@
 module.exports = function(app, config, db, express) {
 
+  var api = require('../routes/api')(express,db);
   var program = require('../routes/program')(express,db);
+  app.use('/api/program', api);
   app.use('/program', program);
 }
