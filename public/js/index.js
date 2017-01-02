d69ba5b4ff6254ebc3b5d497ad24bc1fd2e70ba3
Christopher Impicciche <cimpicci@gmail.com>
2017-01-02 16:12:02 -0600

    adds methods for parsing data from neo4j and passing it to sigma viz

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,28 @@
-// sigma
-//   .parsers
-//   .json(
-//     'myGraph.json', {
-//       container: 'sigma-container'
-//     },
-//     function(sig) {
-//       console.log(sig);
-//     }
-//   );
-
 var app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    path: {
+      nodes: [],
+      edges: []
+    }
+  },
   created: function(){
     this.fetchPaths();
   },
   methods: {
+    runSigma: function() {
+      var self = this;
+      var s = new sigma({
+        graph: self.path,
+        container: 'sigma-container',
+        settings: {
+          defaultNodeColor: '#ec5148',
+          // defaultEdgeColor: '#ec5148'
+        }
+      })
+    },
     fetchPaths: function() {
+      var self = this;
       $.ajax({
         method: 'POST',
         url: 'http://localhost:7474/db/data/transaction/commit',
@@ -29,7 +35,53 @@ var app = new Vue({
           }]
         })
       }).done( result => {
-        console.log(result);
+        // console.log(result);
+        var data = result.results[0].data.map(obj => obj.graph);
+        // console.log(data);
+
+        var nodes = data.reduce((output, graph) => {
+                          graph.nodes.forEach(node => {
+                            output[node.id] = {
+                              id: node.id,
+                              labels: node.labels,
+                              properties: node.properties
+                            }
+                          })
+                          return output;
+                        }, {})
+        var edges = data.reduce((output, graph) => {
+          graph.relationships.forEach(rel => {
+            output[rel.id] = {
+              id: rel.id,
+              source: rel.startNode,
+              target: rel.endNode,
+              color: '#ec5148'
+            }
+          })
+          return output
+        }, {})
+
+        console.log(nodes,edges);
+
+        Object.keys(nodes).forEach(key => {
+          var label = nodes[key].properties.name || nodes[key].properties.title
+          var size = nodes[key].labels[0] === "Program" ? 3 : 2;
+          var color = nodes[key].labels[0] === "Person" ? "#44eeee" : '#ec5148';
+          self.path.nodes.push({
+            id: key,
+            label: label,
+            size: size,
+            x: Math.random(),
+            y: Math.random(),
+            color: color
+          })
+        })
+
+        Object.keys(edges).forEach(key => {
+          self.path.edges.push(edges[key])
+        })
+        // console.log(self.path);
+        self.runSigma();
       })
     }
   }
